291058ef240c21a912094b1161d292cdc1ccdcf8
k-dev <vercelploy@gmail.com>
2025-09-21 19:44:37 +0300

    PWA Display Icon

--- rise-fronted/src/app/layout.js
+++ rise-fronted/src/app/layout.js
@@ -26,6 +26,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* PWA Display Icon */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
       </head>

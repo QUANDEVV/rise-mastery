ac42114eb66ce82d1ed909036ce58f6e4f4b7a5a
k-dev <vercelploy@gmail.com>
2025-10-04 15:48:14 +0300

    Push

--- rise-fronted/src/app/HomePage/layout.js
+++ rise-fronted/src/app/HomePage/layout.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
 // Import the shared Header component using the absolute alias used elsewhere
-import Header from '@/components/Header/Header';
+// import Header from '@/components/Header/Header';
 
 // HomePage layout with shared header and slot for children
 export default function Layout({ children }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       {children}
     </>
   );
